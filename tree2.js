e7a4d924bb937b9d6c6055ac6798d0d092b1de35
bensaxophone <73925085+bensaxophone@users.noreply.github.com>
2020-12-03 20:34:43 -0800

    12/3
    Added comments, got rid of the variable shrinkage.

--- tree2.js
+++ tree2.js
@@ -1,73 +1,76 @@
 var tabs = 7;
 
-/*
-chrome.runtime.sendMessage({method: "getTabs"}, function(response) {
-    tabs = response.status;
-  });
-async function getTabs() {
-    tabs = localStorage["allWindowsTabCount"]
-}
-*/
-
 if(tabs > 26)
   tabs = 26;
+//Decreases leftScale1, rightScale1, etc.
 var health = 0 + 0.002*tabs;
 
 var sway = 0;
 
+// Angle that each branch function branches at:
 var angle1 = 0.5;
 var angle2 = 0.6;
 var angle3 = 0.4;
 
+// Initialized to 0.5, will change in setup.
 var leftScale1 = 0.5;
 var rightScale1 = 0.5;
 var leftScale2 = 0.5;
 var rightScale2 = 0.5;
 var leftScale3 = 0.5;
 var rightScale3 = 0.5;
+// How much longer one branch is than the other:
 var skew = 0.4 * (tabs/26);
 var leftSkew = 0;
 var rightSkew = 0;
 
-var shrinkage = 0;
-var thinness = 6 + tabs;
+
+var thinness = 6 + tabs;  //scales down strikeWeight
+
+//color(will randomize later)
 var r = 0;
 var g = 0;
 var b = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  // Skews to the left/right half of the time
   if(random() > 0.5) {
     rightSkew = 0.8*skew;
   }
   else {
     leftSkew = skew;
   }
+
+  //Randomizes all the scaling variables, within a certain range
   leftScale1 = random(0.65, 0.75) - 6*health - rightSkew;
   rightScale1 = random(0.65, 0.75) - 2*health - leftSkew;
   leftScale2 = random(0.65, 0.8) - 4*health - rightSkew;
   rightScale2 = random(0.65, 0.8) - 3*health - leftSkew;
   leftScale3 = random(0.6, 0.7) - 5*health - rightSkew;
   rightScale3 = random(0.7, 0.8) - 7*health - leftSkew;
+  //Random color, for leaves only in branch3.
   r = random(0, 255);
   g = random(0, 255);
   b = random(0, 255);
+  // only sways if tabs is greater than 6, too slow for larger trees.
   if(tabs < 7)
     noLoop();
 }
 
 function draw() {
+  // Sway changes periodically based on frameCount, makes it look like it's randomly swaying.
   sway = cos(0.02*frameCount)*sin(0.1*frameCount);
   background(255, 204, 200);
   translate(windowWidth/2, height);
-  branch1(windowHeight/4 - shrinkage);
+  branch1(windowHeight/4);
 
 }
 
 function branch1(len) {
-  stroke(82 + (windowHeight/4 - shrinkage - len)*0.1,
-         51 + (windowHeight/4 - shrinkage - len)*0.1,
-         51 + (windowHeight/4 - shrinkage - len)*0.1);
+  stroke(82 + (windowHeight/4 - len)*0.1,
+         51 + (windowHeight/4 - len)*0.1,
+         51 + (windowHeight/4 - len)*0.1);
   strokeWeight(len/thinness);
   line(0,0,0,-len);
   translate(0, -len);
@@ -88,9 +91,9 @@ function branch1(len) {
 }
 
 function branch2(len) {
-  stroke(82 + (windowHeight/4 - shrinkage - len)*0.1,
-         51 + (windowHeight/4 - shrinkage - len)*0.1,
-         51 + (windowHeight/4 - shrinkage - len)*0.1);
+  stroke(82 + (windowHeight/4 - len)*0.1,
+         51 + (windowHeight/4 - len)*0.1,
+         51 + (windowHeight/4 - len)*0.1);
   strokeWeight(len/thinness);
   line(0,0,0,-len);
   translate(0, -len);
@@ -111,9 +114,9 @@ function branch2(len) {
 }
 
 function branch3(len) {
-  stroke(82 + (windowHeight/4 - shrinkage - len)*0.1,
-         51 + (windowHeight/4 - shrinkage - len)*0.1,
-         51 + (windowHeight/4 - shrinkage - len)*0.1);
+  stroke(82 + (windowHeight/4 - len)*0.1,
+         51 + (windowHeight/4 - len)*0.1,
+         51 + (windowHeight/4 - len)*0.1);
   strokeWeight(len/thinness);
   line(0,0,0,-len);
   translate(0, -len);
